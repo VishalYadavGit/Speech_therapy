f64ef25f3bc2bf05a1fe514a84767e0c00aee6e3
xegend <vystudent68@gmail.com>
2025-03-26 23:07:05 +0530

    removed sentences from backend

--- speechtherapy/views.py
+++ speechtherapy/views.py
@@ -5,10 +5,4 @@ def home(request):
     return render(request, 'index.html')
 
 def start_therapy(request):
-    sentences = [
-        "नमस्ते, आप कैसे हैं?",
-        "मुझे पानी चाहिए।",
-        "क्या आप मेरी मदद कर सकते हैं?",
-        "यह एक सुंदर दिन है।"
-    ]
-    return render(request, 'start_therapy.html', {'sentences': sentences})
+    return render(request, 'start_therapy.html',)
